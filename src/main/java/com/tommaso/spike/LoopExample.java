872463c13701c248df0a927e67cd2868bde74af0
Tommaso Torti <tommaso.torti@xpeppers.com>
2019-06-18 08:49:47 +0200

    loop example 2

--- src/main/java/com/tommaso/spike/LoopExample.java
+++ src/main/java/com/tommaso/spike/LoopExample.java
@@ -3,6 +3,8 @@ package com.tommaso.spike;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 
 public class LoopExample {
@@ -11,6 +13,18 @@ public class LoopExample {
         Result result = new Result();
         for (Integer i: input) {
             if(i % 2 == 0) {
+                result.addOk(i);
+            } else {
+                result.addKo(i);
+            }
+        }
+        return result;
+    }
+
+    public Result loopExample2(List<Integer> input, Predicate<Integer> p) {
+        Result result = new Result();
+        for (Integer i: input) {
+            if(p.test(i)) {
                 result.addOk(i);
             } else {
                 result.addKo(i);
--- src/test/java/com/tommaso/spike/LoopExampleTest.java
+++ src/test/java/com/tommaso/spike/LoopExampleTest.java
@@ -11,9 +11,17 @@ import static org.junit.Assert.assertTrue;
 public class LoopExampleTest {
 
     @Test
-    public void loopTest() {
+    public void loopExampleTest() {
         List<Integer> input = Arrays.asList(1, 2, 3);
         Result result = new LoopExample().loopExample(input);
+        assertTrue(result.getOkList().contains(2));
+        assertEquals(Arrays.asList(1,3), result.getKoList());
+    }
+
+    @Test
+    public void loopExample2Test() {
+        List<Integer> input = Arrays.asList(1, 2, 3);
+        Result result = new LoopExample().loopExample2(input, ((Integer i) -> i % 2 == 0));
         assertTrue(result.getOkList().contains(2));
         assertEquals(Arrays.asList(1,3), result.getKoList());
     }
